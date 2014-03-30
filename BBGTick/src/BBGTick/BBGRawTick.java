254d5b82458dd84862c99fc6d86ccb0d96a0d6c8
fuheng wu <wufuheng@gmail.com>
2014-03-30 18:31:37 +0800

    u

--- BBGTick/src/BBGTick/BBGRawTick.java
+++ BBGTick/src/BBGTick/BBGRawTick.java
@@ -1,5 +1,6 @@
 package BBGTick;
-// -sd 2012-11-01T05:30:00 -ed 2012-11-02T20:30:00
+//-sd 2012-11-01T05:30:00 -ed 2012-11-02T20:30:00
+//http://stackoverflow.com/questions/8559165/converting-date-from-utc-to-est-in-java
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -15,12 +16,19 @@ import com.bloomberglp.blpapi.SessionOptions;
 import com.bloomberglp.blpapi.Request;
 import com.bloomberglp.blpapi.Service;
 import com.bloomberglp.blpapi.Session;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.io.PrintWriter;
 
 public class BBGRawTick {
     private static final Name TICK_DATA      = new Name("tickData");
     private static final Name COND_CODE      = new Name("conditionCodes");
+    private static final Name EXCH_CODE      = new Name("exchangeCodes");
     private static final Name SIZE           = new Name("size");
     private static final Name TIME           = new Name("time");
     private static final Name TYPE           = new Name("type");
@@ -39,23 +47,88 @@ public class BBGRawTick {
     private SimpleDateFormat  d_dateFormat;
     private DecimalFormat     d_decimalFormat;
     private PrintWriter writer;
+    private boolean ishk;
+    private String d_dir;
 
     public static void main(String[] args) throws Exception
     {
         BBGRawTick brt = new BBGRawTick();
+        brt.getSec();
         brt.run(args);
         //System.out.println("Press ENTER to quit");
         //System.in.read();
     }
+    
+    private void getSec() throws Exception {
+        d_secs = new ArrayList<String>();
+        ishk = false;
+        try {
+            if (ishk) {
+                d_dir = "hkse-main2";
+                //Scanner s = new Scanner(new File("hkse-main.csv"));
+                FileInputStream fstream = new FileInputStream("hkse-main.csv");
+                //ArrayList<String> list = new ArrayList<String>();
+                DataInputStream in = new DataInputStream(fstream);
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String line;
+                int i = 0;
+                while ((line = br.readLine()) != null) {
+                    i++;
+                    if (i < 2) {
+                        continue;
+                    }
+                    String[] parts = line.split(",");
+                    String sec = parts[0];
+                    sec = sec.replaceFirst("^0+(?!$)", "");
+                    sec = sec.replace("\"", "");
+                    String filePathString="F:\\uploaded\\"+sec+".HK.csv";
+                    File f = new File(filePathString);
+                    if(!f.exists()) {
+                        d_secs.add(sec);
+                    }
+                    //System.out.println(part1);
+                }
+                in.close();
+            } else {
+                //d_dir = "nasdaq-china";
+                d_dir = "henry";
+                //FileInputStream fstream = new FileInputStream("nasdaq-china.csv");
+                FileInputStream fstream = new FileInputStream("henry.lst");
+                // Get the object of DataInputStream
+                DataInputStream in = new DataInputStream(fstream);
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String line;
+                //Read File Line By Line
+                int i = 0;
+                while ((line = br.readLine()) != null) {
+                    i++;
+                    if (i < 2) {
+                        continue;
+                    }
+                    // Print the content on the console
+                    //System.out.println (strLine);
+                    String[] parts = line.split(",");
+                    String sec = parts[0];
+                    sec = sec.replaceFirst("^0+(?!$)", "");
+                    sec = sec.replace("\"", "");
+                    d_secs.add(sec);
+                }
+                //Close the input stream
+                in.close();
+            }
 
-    private Calendar getPreviousTradingDate()
-    {
+
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private Calendar getPreviousTradingDate(){
         Calendar rightNow = Calendar.getInstance();
         rightNow.roll(Calendar.DAY_OF_MONTH, -1);
         if (rightNow.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
             rightNow.roll(Calendar.DAY_OF_MONTH, -2);
-        }
-        else if (rightNow.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+        }else if (rightNow.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
             rightNow.roll(Calendar.DAY_OF_MONTH, -1);
         }
 
@@ -78,15 +151,16 @@ public class BBGRawTick {
         //d_security = "GOOG US 11/17/12 C680 EQUITY";//option
         //d_security = "BITA US 03/22/14 C35 Equity";
         d_secs = new ArrayList<String>();
-        d_secs.add("BITA US Equity");
+        //d_secs.add("BITA US Equity");
         d_events = new ArrayList<String>();
         d_dateFormat = new SimpleDateFormat();
-        d_dateFormat.applyPattern("MM/dd/yyyy k:mm:ss");
+        d_dateFormat.applyPattern("yyyy-MM-dd HH:mm:ss");
+        // k	hour in day (1-24)	Number	24
         d_decimalFormat = new DecimalFormat();
         d_decimalFormat.setMaximumFractionDigits(3);
         
-        d_startDateTime="2013-12-01T00:00:00";
-        d_endDateTime="2014-04-16T00:00:00";
+        d_startDateTime="2013-03-22T00:00:00";
+        d_endDateTime="2014-03-30T00:00:00";
     }
 
     private void run(String[] args) throws Exception
@@ -112,15 +186,14 @@ public class BBGRawTick {
         //TODO: Read stocks data into d_secs!!!
         
         for (String security : d_secs){
-            writer = new PrintWriter(security+".csv", "UTF-8");
-            writer.println("TIME,TYPE,VALUE,SIZE,CC");
+            //writer = new PrintWriter(security+".csv", "UTF-8");//new FileOutputStream(new File("persons.txt"),true)
+            writer = new PrintWriter(new FileOutputStream(new File("henry\\"+security+".csv"),true));
+            //writer.println("TIME,TYPE,VALUE,SIZE,CC");
             sendIntradayTickRequest(session, security);
             // wait for events from session.
             eventLoop(session);
             writer.close();
         }
-        
-        
         session.stop();
     }
 
@@ -151,12 +224,16 @@ public class BBGRawTick {
             }
         }
     }
+    
+    private String RMComma(String s, String to){
+        return s.replace(",", to);
+    }
 
     private void processMessage(Message msg) throws Exception {
         Element data = msg.getElement(TICK_DATA).getElement(TICK_DATA);
         int numItems = data.numValues();
-        System.out.println("TIME\t\t\tTYPE\tVALUE\t\tSIZE\tCC");
-        System.out.println("----\t\t\t----\t-----\t\t----\t--");
+        System.out.println("TIME\t\t\tTYPE\t\tVALUE\t\tSIZE\tCC");
+        System.out.println("----\t\t\t----\t\t-----\t\t----\t--");
 
         for (int i = 0; i < numItems; ++i) {
             Element item = data.getValueAsElement(i);
@@ -164,23 +241,33 @@ public class BBGRawTick {
             String type = item.getElementAsString(TYPE);
             double value = item.getElementAsFloat64(VALUE);
             int size = item.getElementAsInt32(SIZE);
-            String cc = "";
-            if (item.hasElement(COND_CODE)) {
-                cc = item.getElementAsString(COND_CODE);
+            if (value==0 && size==0){
+                continue;
             }
+            String cc = "";
+            String exch = "";
+            if (item.hasElement(COND_CODE)) {
+                cc = RMComma(item.getElementAsString(COND_CODE),"|");
+                // cc - composite close; oc - official local exchange close
+                // R6,IS - rule 611 trade, inter market sweep order, OL - odd lot trade
+                // T - nasdaq trade reporting facility
+                // 6 - market center closing trade
+                // CL - closing quotation
+            }
+            
 
             String t=d_dateFormat.format(time.calendar().getTime());
             String ty=type;
-            String val=d_decimalFormat.format(value);
-            String sz=d_decimalFormat.format(size);
+            String val=RMComma(d_decimalFormat.format(value),"");
+            String sz=RMComma(d_decimalFormat.format(size),"");
 
             writer.println(t+","+ty+","+val+","+sz+","+cc);
-
-            System.out.println(d_dateFormat.format(time.calendar().getTime()) + "\t" +
-                    type + "\t" +
-                    d_decimalFormat.format(value) + "\t\t" +
-                    d_decimalFormat.format(size) + "\t" +
-                    cc);
+            
+            if (item.hasElement(EXCH_CODE)) {
+                exch = RMComma(item.getElementAsString(EXCH_CODE),"|");
+                System.out.println(t+"\t"+ty+"\t"+val+"\t\t"+sz+"\t"+cc+"\t"+exch);
+            }
+            //System.out.println(t+"\t"+ty+"\t"+val+"\t\t"+sz+"\t"+cc+"\t"+exch);
         }
     }
 
@@ -201,7 +288,8 @@ public class BBGRawTick {
         Service refDataService = session.getService("//blp/refdata");
         Request request = refDataService.createRequest("IntradayTickRequest");
 
-        request.set("security", sec);
+        //request.set("security", sec);
+        request.set("security", sec+ (ishk?" HK EQUITY":" US EQUITY"));
 
         // Add fields to request
         Element eventTypes = request.getElement("eventTypes");
@@ -210,8 +298,10 @@ public class BBGRawTick {
         }
         eventTypes.appendValue("BEST_BID");//henry
         eventTypes.appendValue("BEST_ASK");
+        //eventTypes.appendValue("BID");
+        //eventTypes.appendValue("ASK");
 
-        // All times are in GMT
+        // All times are in GMT/UTC
         request.set("startDateTime", d_startDateTime);
         request.set("endDateTime", d_endDateTime);
         request.set("includeConditionCodes", true);
